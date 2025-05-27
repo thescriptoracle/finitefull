74847ae8ae37a207eef38f1cc1c17c3483ee9712
Haybe <fatokihabeebolamilekan3431@gmail.com>
2025-05-27 17:51:52 +0100

    Update NewsletterBox.jsx

--- frontend/src/components/NewsletterBox.jsx
+++ frontend/src/components/NewsletterBox.jsx
@@ -10,7 +10,6 @@ const NewsletterBox = () => {
     <div className=' text-center'>
       <p className='text-2xl font-medium text-gray-800'>Subscribe now & get 20% off</p>
       <p className='text-gray-400 mt-3'>
-      Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
       </p>
       <form onSubmit={onSubmitHandler} className='w-full sm:w-1/2 flex items-center gap-3 mx-auto my-6 border pl-3'>
         <input className='w-full sm:flex-1 outline-none' type="email" placeholder='Enter your email' required/>
