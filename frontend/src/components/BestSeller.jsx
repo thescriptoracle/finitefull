787cd4c66fd5d0e36663261b7e0b5b022719b29d
Haybe <fatokihabeebolamilekan3431@gmail.com>
2025-05-27 17:51:18 +0100

    Update BestSeller.jsx

--- frontend/src/components/BestSeller.jsx
+++ frontend/src/components/BestSeller.jsx
@@ -18,7 +18,7 @@ const BestSeller = () => {
       <div className='text-center text-3xl py-8'>
         <Title text1={'BEST'} text2={'SELLERS'}/>
         <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the.
+        Shop our best-selling pieces loved by the Finiteluxury community. These customer favorites represent the perfect balance of luxury, style, and quality, for those who appreciate refined fashion.
         </p>
       </div>
 
