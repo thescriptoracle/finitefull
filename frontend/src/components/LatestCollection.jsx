b71fdf0b6f8af8a16ce59ccb32dc07dfc68ad6ca
Haybe <fatokihabeebolamilekan3431@gmail.com>
2025-05-27 17:50:18 +0100

    Update LatestCollection.jsx

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -17,7 +17,7 @@ const LatestCollection = () => {
       <div className='text-center py-8 text-3xl'>
           <Title text1={'LATEST'} text2={'COLLECTIONS'} />
           <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the.
+          Discover our latest fashion collections designed to redefine elegance.
           </p>
       </div>
 
