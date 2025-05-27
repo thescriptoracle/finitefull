44a05fdd3906fb0b2b1a9d27baed0956ca7d5806
Haybe <fatokihabeebolamilekan3431@gmail.com>
2025-05-27 17:55:30 +0100

    Update Contact.jsx

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -15,9 +15,8 @@ const Contact = () => {
         <img className='w-full md:max-w-[480px]' src={assets.contact_img} alt="" />
         <div className='flex flex-col justify-center items-start gap-6'>
           <p className='font-semibold text-xl text-gray-600'>Our Store</p>
-          <p className=' text-gray-500'>54709 Willms Station <br /> Suite 350, Washington, USA</p>
-          <p className=' text-gray-500'>Tel: (415) 555-0132 <br /> Email: admin@forever.com</p>
-          <p className='font-semibold text-xl text-gray-600'>Careers at Forever</p>
+          <p className=' text-gray-500'>WhatsApp: (+234) 903-312-0032 <br /> Email: finiteluxurywears@gmail.com</p>
+          <p className='font-semibold text-xl text-gray-600'>Careers at Finiteluxury</p>
           <p className=' text-gray-500'>Learn more about our teams and job openings.</p>
           <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
         </div>
